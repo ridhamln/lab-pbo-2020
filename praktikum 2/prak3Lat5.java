c77bab8bc27c83b4d5a48b7247a90f8c146612d7
Muhammad Ridha Maulana <mrmaulana1003@gmail.com>
2020-10-15 21:48:31 +0700

    Membuat Kode Pertemuan 2

---
+++ praktikum 2/prak3Lat5.java
@@ -0,0 +1,11 @@
+/*
+Program untuk mendapatkan nilai argumen pada method main
+*/
+
+public class prak3Lat5 {
+public static void main(String args[]) {
+System.out.println("Tanggal : " + args[0]);
+System.out.println("Bulan : " + args[1]);
+System.out.println("Tahun : " + args[2]);
+}
+}
